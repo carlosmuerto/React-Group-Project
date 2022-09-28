9a748322f579c945b382b613c5d177ed56a65a2a
Carlos Vivas <carlosmuerto@gmail.com>
2022-09-28 11:42:01 -0400

    fix rocket page comple problem

--- src/view/components/Rocket/rocket.jsx
+++ src/view/components/Rocket/rocket.jsx
@@ -1,32 +1,38 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-function Rocket({ rocket }) {
+const Rocket = ({ rocket }) => {
   const [reserved, setReserved] = useState(false);
-  const { id, rocket_name, description, flickr_images,  } = rocket;
-  const [image, setImage] = useState(flickr_images[0]);
+  const {
+    id, rocketName, description, flickrImages,
+  } = rocket;
+  const [image, setImage] = useState(flickrImages[0]);
   return (
     <div className="rocket-card">
-      <img src={image} alt={rocket_name} />
+      <img src={image} alt={rocketName} />
       <div className="rocket-info">
-        <h2>{rocket_name}</h2>
-        <p>{reserved? (<p className="reserve">Reserved</p>): null}{description}</p>
+        <h2>{rocketName}</h2>
+        <p>
+          {reserved ? (<p className="reserve">Reserved</p>) : null}
+          {description}
+        </p>
         {/* reserve button */}
-        <button type="button" className={reserved? 'cancel': 'reserved'} onClick={() => setReserved(!reserved)}>
-            {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        <button type="button" className={reserved ? 'cancel' : 'reserved'} onClick={() => setReserved(!reserved)}>
+          {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
         </button>
       </div>
     </div>
   );
-}
+};
 
 Rocket.propTypes = {
-    rocket: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        rocket_name: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
-    }).isRequired,
+  rocket: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    rocketName: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    flickrImages: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
 };
 
 export default Rocket;
--- src/view/pages/Rockets/Rockets.jsx
+++ src/view/pages/Rockets/Rockets.jsx
@@ -1,23 +1,24 @@
-import { useState, useEffect } from 'react';
-import Rocket from '../components/Rocket';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchRockets } from '../redux/rocketsReducer';
+import Rocket from '../../components/Rocket/rocket';
+import rocketsListMock from './RocketMock';
 
-const RocketsPage = () => (
-  const rockets = useSelector((state) => state.rockets.rockets);
+const RocketsPage = () => {
+  const rockets = rocketsListMock.map((rocketMock) => ({
+    id: rocketMock.id,
+    rocketName: rocketMock.rocket_name,
+    description: rocketMock.description,
+    flickrImages: rocketMock.flickr_images,
+  }));
+  // eslint-disable-next-line no-console
   console.log(rockets);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, []);
 
   return (
-      <div className="rockets-container">
-      {rockets.map((rocket,index) => (
-          <Rocket key={index} rocket={rocket} />
+    <div className="rockets-container">
+      {rockets.map((rocket) => (
+        <Rocket key={`rockets-item-${rocket.id}`} rocket={rocket} />
       ))}
-      </div>
+    </div>
   );
-
+};
+/*
+*/
 export default RocketsPage;

6b1692fef499a940fddce39d8efbfea78c12bb1b
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-09-29 19:53:23 +0200

    Display fetched rocket

--- src/view/pages/Rockets/Rockets.jsx
+++ src/view/pages/Rockets/Rockets.jsx
@@ -1,25 +1,19 @@
-import Rocket from '../../components/Rocket/rocket';
-import rocketsListMock from './RocketMock';
+import fetchRocket from '../../../features/rockets/RocketAPI';
 import './rocket.css';
 
-const RocketsPage = () => {
-  const rockets = rocketsListMock.map((rocketMock) => ({
-    id: rocketMock.id,
-    rocketName: rocketMock.rocket_name,
-    description: rocketMock.description,
-    flickrImages: rocketMock.flickr_images,
-  }));
-  // eslint-disable-next-line no-console
-  console.log(rockets);
-
-  return (
-    <div className="rockets-container">
-      {rockets.map((rocket) => (
-        <Rocket key={`rockets-item-${rocket.id}`} rocket={rocket} />
-      ))}
+const RocketsPage = () => (
+  <div className="rockets-container">
+    <fetchRocket />
     </div>
-  );
-};
-/*
-*/
+);
+
 export default RocketsPage;
+
+
+
+
+
+
+
+
+

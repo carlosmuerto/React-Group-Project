dbb22ac847fc09b16e6ddd89804004d1b98bb606
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-09-29 19:54:02 +0200

    using fetched data

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from '../features/mission/MissionSlice';
+import rocketsReducer from '../features/rockets/RocketSlice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketsReducer
   },
 });
 
